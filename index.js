ebf7022fffda7b43430d6d9bba3672e20aa62769
Karem1986 <karem.ortiz@outlook.com>
2020-06-25 15:58:17 +0200

    uploaded rign excluding node_modules

---
+++ index.js
@@ -0,0 +1,7 @@
+const toTitledCase = require('to-title-case')
+console.log(toTitledCase)
+
+//Conver a title 
+const lower = 'the lord of the rings: the return of the king'
+const title = toTitledCase(lower)
+console.log(title)
